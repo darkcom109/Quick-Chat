1ac8445f187889ee64e9df79fcc0c0e211c4af56
darkcom109 <garciavalentino2006@gmail.com>
2025-09-02 20:49:07 +0100

    Fixed the issue with user having access to talk to themselves

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -7,7 +7,7 @@ import {io, userSocketMap} from "../server.js"
 
 export const getUsersForSidebar = async (req, res) => {
     try {
-        const userId = req.user._Id;
+        const userId = req.user._id;
         const filteredUsers = await User.find({_id: {$ne: userId}}).select("-password");
 
         // Count number of messages not seen
